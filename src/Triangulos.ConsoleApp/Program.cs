cf7594577362e431b94059175f08924574efefab
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-04-08 17:36:02 -0300

    Resolução do exercicio

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Triangulos.ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿namespace Triangulos.ConsoleApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Triangulo tri = new Triangulo();
+            while (true) {
+                try {
+                    Console.WriteLine("Informe o valor de X:");
+                    tri.x = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe o valor de Y:");
+                    tri.y = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe o valor de Z:");
+                    tri.z = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Resultado:");
+                    tri.VerificaCategoriaTriangulo();
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Apenas numeros são aceitos!!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            });
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,47 @@
+﻿namespace Triangulos.ConsoleApp {
+    internal class Triangulo {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        private bool _ehEquilatero(int x, int y, int z) {
+            if (x == y && y == z)
+                return true;
+            else
+                return false;
+        }
+        private bool _ehIsosceles(int x, int y, int z) {
+            if (x == y || y == z || x == z)
+                return true;
+            else
+                return false;
+        }
+        private bool _ehEscaleno(int x, int y, int z) {
+            if (x != y && y != z && x != z)
+                return true;
+            else
+                return false;
+        }
+        private bool _ehTriangulo(int x, int y, int z) {
+            if ((x + y) > z && (x + z) > y && (z + y) > x)
+                return true;
+            else
+                return false;
+        }
+        public void VerificaCategoriaTriangulo() {
+            if (_ehTriangulo(x, y, z)) {
+                if (_ehEquilatero(x, y, z)) {
+                    Console.WriteLine("O triangulo é Equilátero");
+                }
+                else if (_ehIsosceles(x, y, z)) {
+                    Console.WriteLine("O triangulo é Isosceles");
+                }
+                else if (_ehEscaleno(x, y, z)) {
+                    Console.WriteLine("O triangulo é Escaleno");
+                }
+            }
+            else {
+                Console.WriteLine("Não é possível formar um triangulo com tais medidas!");
+            }
+        }
+    }
+}
